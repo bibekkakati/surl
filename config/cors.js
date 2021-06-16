19d92fe0e52b75828f85c8c755e5f795cf6a0a3e
bibekkakati <bibekkakati0@gmail.com>
2021-06-16 18:07:29 +0530

    Fixes

--- config/cors.js
+++ config/cors.js
@@ -4,7 +4,7 @@ module.exports = cors({
 	origin:
 		process.env.NODE_ENV === "development"
 			? "http://localhost:" + process.env.PORT
-			: "https://ursurl.herokuapp.com",
+			: "https://surl.bibekkakati.me",
 	optionsSuccessStatus: 200,
 	methods: ["GET"],
 	maxAge: 3 * 60,
