e35c80c1d4ce7c7c004fffde1a792384123587f9
bibekkakati <bibekkakati0@gmail.com>
2021-06-21 21:16:40 +0530

    Socket config changed

--- config/request.js
+++ config/request.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 
 const Agent = require("agentkeepalive");
 const keepAliveAgent = new Agent({
-	maxSockets: 1000,
-	maxFreeSockets: 256,
+	maxSockets: 50,
+	maxFreeSockets: 10,
 	keepAlive: true,
 	timeout: 60000 * 15, // active socket keepalive for 15 minutes
 	freeSocketTimeout: 30000, // free socket keepalive for 30 seconds
